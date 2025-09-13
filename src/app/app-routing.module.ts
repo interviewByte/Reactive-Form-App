b3edecf909bd8eb07877860ceed4f2f26fc6abe9
Pavan Kumar Singh <pavanrnri1818@gmail.com>
2025-09-13 19:46:09 +0530

    dynamic form creted successfully

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,24 @@ import { TodosComponent } from './components/todos/todos.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { RegigterComponent } from './components/regigter/regigter.component';
 import { SelectFormComponent } from './components/select-form/select-form.component';
+import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
 
 const routes: Routes = [
   { path: '', component: TodosComponent },
   { path: 'product', component: ProductListComponent },
   { path: 'register', component: RegigterComponent },
   { path: 'select', component: SelectFormComponent },
-  { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) },
-  { path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) },
+  { path: 'dynamic-form', component: DynamicFormComponent },
+  {
+    path: 'customers',
+    loadChildren: () =>
+      import('./customers/customers.module').then((m) => m.CustomersModule),
+  },
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('./orders/orders.module').then((m) => m.OrdersModule),
+  },
 ];
 
 @NgModule({
--- src/app/components/dynamic-form/dynamic-form.component.ts
+++ src/app/components/dynamic-form/dynamic-form.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-form',
   templateUrl: './dynamic-form.component.html',
-  styleUrls: ['./dynamic-form.component.css']
+  styleUrls: ['./dynamic-form.component.css'],
 })
-export class DynamicFormComponent {
-
+export class DynamicFormComponent implements OnInit {
+  dynamicFormData: any[] = [
+    { key: 'name', type: 'text', value: 'Pavan' },
+    { key: 'email', type: 'email', value: 'pavan@example.com' },
+    { key: 'age', type: 'number', value: 25 },
+  ];
+  dynamicForm!: FormGroup;
+  ngOnInit(): void {
+    this.createForm(this.dynamicFormData);
+  }
+  createForm(fileds: any) {
+    const group: any = {};
+    fileds.forEach((element: any) => {
+      group[element.key] = new FormControl(element.value || '');
+    });
+    this.dynamicForm = new FormGroup(group);
+  }
+  handleSubmit() {
+    console.log(this.dynamicForm.value);
+  }
 }
